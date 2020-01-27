b67afc89c61666aa3b2e1ed8c547fa63ac21f092
koguntu1 <ifon2@msn.com>
2020-01-27 18:49:02 +0000

    Installed and configured mongoose, setup vacationhome model, set up model inside the routes

--- app.js
+++ app.js
@@ -1,32 +1,68 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose")
 
+mongoose.set("useUnifiedTopology", true); 
+mongoose.connect("mongodb://localhost/vacation_home", {useNewUrlParser: true}); 
+//mongoose.connect("mongodb://localhost:27017/vacation_home"), {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+// SCHEMA SETUP
+var vacationhomeSchema = new mongoose.Schema({
+   name: String,
+   image: String,
+   description: String
+});
 
-var vacationhomes = [
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
-];
+var Vacationhome = mongoose.model("Vacationhome", vacationhomeSchema);
 
+ Vacationhome.create(
+     {
+    //      name: "Granite Hill", 
+    //      image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
+    //      description: "This is a huge granite hill, no bathrooms.  No water. Beautiful granite!"
+         
+     },
+     function(err, vacationhome){
+      if(err){
+          console.log(err);
+      } else {
+          console.log("NEWLY CREATED VACATIONHOME: ");
+          console.log(vacationhome);
+      }
+    });
+
+// var vacationhomes = [
+//         {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+//         {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+//         {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+//         {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+//         {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+//         {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+//         {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+//         {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+//         {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
+// ];
 
 app.get("/", function(req, res){
     res.render("home");
 });
 
+//INDEX - show all vacationhomes
 app.get("/vacationhomes", function(req, res){
-    res.render("vacationhomes",{vacationhomes:vacationhomes});
+	// Get all vacationhomes from DB
+    Vacationhome.find({}, function(err, allVacationhomes){
+       if(err){
+           console.log(err);
+       } else {
+    res.render("vacationhomes",{vacationhomes:allVacationhomes});
+		   }
+    });
 });
 
+//CREATE - add new vacationhome to DB
 app.post("/vacationhomes", function(req, res){
     // get data from form and add to vacationhomes array
     var name = req.body.name;
