28f11be624d62e457c7aac951ca6594903bab8bd
koguntu1 <ifon2@msn.com>
2020-02-11 11:24:35 +0000

    Modified router/index.js, changed render to redirect register to handle signup logic

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.post("/register", function(req, res){
     User.register(newUser, req.body.password, function(err, user){
         if(err){
             req.flash("error", err.message);
-            return res.render("register");
+            return res.redirect("./register");
         }
         passport.authenticate("local")(req, res, function(){
 			req.flash("success", "Welcome to YelpCamp " + user.username);
